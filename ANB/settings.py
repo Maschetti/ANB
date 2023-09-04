9f14199e01c57a520dc170c021c9008b9463607d
ReformedTheo <lopes.theo22@gmail.com>
2023-09-04 11:42:55 -0300

    Medical survey model

--- ANB/settings.py
+++ ANB/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Disable prefixing relative URLs with request.path, handle as absolute file paths
+WEASYPRINT_BASEURL = '/'
